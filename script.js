71fee0b90baa89803af72d3235913aed36c95c2f
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-02-16 10:27:34 +0530

    accordian

--- script.js
+++ script.js
@@ -98,3 +98,40 @@ function displayCountryContentt(countryy) {
     rightBox.style.justifyContent = "center";
     rightBox.style.alignItems = "center";
 }
+
+
+// for laptop size 
+document.addEventListener("DOMContentLoaded", function () {
+    var actions = document.querySelectorAll(".action");
+    actions.forEach(function (action) {
+        action.addEventListener("click", function (e) {
+            e.preventDefault();
+            var slidess = document.querySelectorAll(".slideLi");
+            slidess.forEach(function (slideLi) {
+                slideLi.classList.remove("active");
+            });
+            var slideLi = this.closest(".slideLi");
+            slideLi.classList.add("active");
+        });
+    });
+
+    function checkWidth() {
+        var windowSize = window.innerWidth;
+        if (windowSize > 480) {
+            var slideWidth = document.querySelector(".action").offsetWidth;
+            var slideContents = document.querySelectorAll(".slide-contentt");
+            slideContents.forEach(function (slideContentt) {
+                slideContent.style.width = slideWidth + "px";
+            });
+        }
+    }
+
+    window.addEventListener("resize", function () {
+        checkWidth();
+        clearTimeout(window.resizedFinished);
+        window.resizedFinished = setTimeout(checkWidth, 1000);
+    });
+
+    // Initial check on load
+    checkWidth();
+});
